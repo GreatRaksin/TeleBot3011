2b3ce1d381149d355fd69421b6dc2b51fc5b80bb
architect <greatraksin@icloud.com>
2019-12-07 16:42:26 +0300

    initial commit

---
+++ mi.py
@@ -0,0 +1,49 @@
+aa = {
+         'content_type': 'sticker',
+         'message_id': 75,
+         'from_user': {
+             'id': 115943804,
+             'is_bot': False,
+             'first_name': 'Great',
+             'username': 'great_raksin',
+             'last_name': 'Raksin',
+             'language_code': 'en'
+         },
+         'date': 1575185819,
+         'chat': {
+             'type': 'private',
+             'last_name': 'Raksin',
+             'first_name': 'Great',
+             'username': 'great_raksin',
+             'id': 115943804,
+             'title': None,
+             'all_members_are_administrators': None,
+             'photo': None,
+             'description': None,
+             'invite_link': None,
+             'pinned_message': None,
+             'sticker_set_name': None,
+             'can_set_sticker_set': None
+         },
+         'forward_from_chat': None,
+         'forward_from': None,
+         'forward_date': None,
+         'reply_to_message': None,
+         'edit_date': None,
+         'media_group_id': None,
+         'author_signature': None,
+         'text': None,
+         'entities': None,
+         'caption_entities': None,
+         'audio': None,
+         'document': None,
+         'photo': None,
+
+         'sticker': {
+                        'file_id': 'CAADAgADehsAAkKvaQABFl4pYmM5fOQWBA',
+                        'width': 512,
+                        'height': 512,
+                        'thumb': <telebot.types.PhotoSize object at 0x10766deb8>,
+                        'emoji': '🎈',
+                        'set_name': 'vk_poop', 'mask_position': None, 'file_size': 22814}, 'video': None, 'video_note': None, 'voice': None, 'caption': None, 'contact': None, 'location': None, 'venue': None, 'new_chat_member': None, 'new_chat_members': None, 'left_chat_member': None, 'new_chat_title': None, 'new_chat_photo': None, 'delete_chat_photo': None, 'group_chat_created': None, 'supergroup_chat_created': None, 'channel_chat_created': None, 'migrate_to_chat_id': None, 'migrate_from_chat_id': None, 'pinned_message': None, 'invoice': None, 'successful_payment': None, 'connected_website': None, 'json': {'message_id': 75, 'from': {'id': 115943804, 'is_bot': False, 'first_name': 'Great', 'last_name': 'Raksin', 'username': 'great_raksin', 'language_code': 'en'}, 'chat': {'id': 115943804, 'first_name': 'Great', 'last_name': 'Raksin', 'username': 'great_raksin', 'type': 'private'}, 'date': 1575185819, 'sticker': {'width': 512, 'height': 512, 'emoji': '🎈', 'set_name': 'vk_poop', 'is_animated': False, 'thumb': {'file_id': 'AAQCAAN6GwACQq9pAAEWXiliYzl85O9w4w0ABAEAB20AA7geAAIWBA', 'file_size': 5112, 'width': 128, 'height': 128}, 'file_id': 'CAADAgADehsAAkKvaQABFl4pYmM5fOQWBA', 'file_size': 22814}}}
+<class 'telebot.types.Message'>
